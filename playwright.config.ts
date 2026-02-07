17a118b1e5564cc1142f4c94496a9b0e4718706a
Anderson Cahet <anderson.cahet.it@gmail.com>
2026-02-07 19:31:40 +0100

    add allure reports with screenshot and videos

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
-   ['list']
+   ['list'],['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -32,7 +32,11 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+   screenshot: 'only-on-failure',
+   viewport: { width: 1280, height: 720 },
+   ignoreHTTPSErrors: true,
   },
 
   /* Configure projects for major browsers */
